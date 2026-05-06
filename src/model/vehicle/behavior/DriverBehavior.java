7172118d9fcd5509d5fb2be8572c47e8f63ecb63
Mhbb1234 <duongtuanphi28092006@gmail.com>
2026-05-06 22:48:07 +0700

    Hoàn thành Behavior

--- src/model/vehicle/behavior/DriverBehavior.java
+++ src/model/vehicle/behavior/DriverBehavior.java
@@ -3,19 +3,69 @@ package model.vehicle.behavior;
 import model.vehicle.Vehicle;
 
 public abstract class DriverBehavior {
-	
-	protected double sightDistance;
-	
-	  public void decide(Vehicle self, Vehicle ahead, double dist, boolean isRed) {
-		  if (self.isEmergency()) handleEmergency(self);
-		  else if (isRed && dist < sightDistance) handleRedLight(self, dist);  
-		  else if (ahead != null) handleFollowVehicle(self, ahead);
-		  else handleFreeWay(self);
-	  }
-	  
-	  protected abstract void handleFreeWay (Vehicle self);
-	  protected abstract void  handleFollowVehicle (Vehicle self, Vehicle ahead);
-	  protected abstract void handleRedLight (Vehicle self, double distance);
-	  protected abstract void handleEmergency (Vehicle self);
-      public abstract String getBehaviorName();
+    protected double speedRatio;     
+    protected double safeTimeGap;    
+    protected double accNormal;      
+    protected double accStrong;      
+    protected double brakeNormal;   
+    protected double brakeStrong;    
+    protected double sightDistance; 
+    protected double overtakeThreshold;
+
+    public void decide(Vehicle self, Vehicle ahead, double distToLight, boolean isRed) {
+        double freeWayAcc = handleFreeWay(self);
+        double followAcc = handleFollowVehicle(self, ahead);
+        double lightAcc = handleRedLight(self, distToLight, isRed);
+        double finalAcc = Math.min(freeWayAcc, Math.min(followAcc, lightAcc));
+        handleLaneChange(self, ahead);
+        self.applyAcceleration(finalAcc);
+    }
+
+    protected double calculateBrakeToStop(double currentSpeed, double distance) {
+        if (distance <= 0.5) return 0;
+        double targetAcc = -(currentSpeed * currentSpeed) / (2 * distance);
+        return Math.max(brakeStrong, targetAcc);
+    }
+    
+    protected void attemptLaneChange(Vehicle self) {
+        if (self.getCurrentLane().getNeighborLane(-1) != null) self.changeLane(-1);
+        else if (self.getCurrentLane().getNeighborLane(1) != null) self.changeLane(1);
+    }
+
+    protected double handleFreeWay(Vehicle self) {
+        double targetSpeed = self.getMaxSpeed() * this.speedRatio;
+        if (self.getSpeed() < targetSpeed) return this.accNormal;
+        else if (self.getSpeed() > targetSpeed) return this.brakeNormal;
+        return 0.0;
+    }
+    
+    protected double handleFollowVehicle(Vehicle self, Vehicle ahead) {
+    	if (ahead == null) return Double.MAX_VALUE;
+    	
+        double dist = self.getPosition().distanceTo(ahead.getPosition());
+        double safetyGap = self.getSpeed() * this.safeTimeGap;
+
+        if (dist < safetyGap)
+            return (self.getSpeed() - ahead.getSpeed()) > 0 ? 
+            		this.brakeStrong : this.brakeNormal;
+        return this.accNormal;
+    }
+    
+    protected double handleRedLight(Vehicle self, double distance, boolean isRed) {
+        if (!isRed || distance > sightDistance) return Double.MAX_VALUE;
+        return this.calculateBrakeToStop(self.getSpeed(), distance);
+    }
+    
+    protected void handleLaneChange(Vehicle self, Vehicle ahead) {
+    	if (ahead != null) {
+            double dist = self.getPosition().distanceTo(ahead.getPosition());
+            double targetSpeed = self.getMaxSpeed() * this.speedRatio;
+            
+            if (dist < targetSpeed * this.safeTimeGap && ahead.getSpeed() < targetSpeed * this.overtakeThreshold) {
+                attemptLaneChange(self);
+            }
+        }
+    }
+    
+    public abstract String getBehaviorName();
 }
--- src/model/vehicle/behavior/NormalDriver.java
+++ src/model/vehicle/behavior/NormalDriver.java
@@ -1,66 +1,19 @@
 package model.vehicle.behavior;
 
-import model.vehicle.Vehicle;
-
 public class NormalDriver extends DriverBehavior {
-	    protected static final double SPEED_RATIO_NORMAL = 0.8;
-	    protected static final double SAFE_TIME_GAP = 2.0; 
-	    protected static final double ACC_NORMAL = 1.5;
-	    protected static final double BRAKE_NORMAL = -1.0;
-	    protected static final double BRAKE_STRONG = -2.5;
-	    protected static final double DIST_BRAKE_ZONE = 40.0;
-
     public NormalDriver() {
-    	this.sightDistance = 150;
+        this.speedRatio = 0.8;
+        this.safeTimeGap = 2.0;
+        this.accStrong = 2.0;
+        this.accNormal = 1.0;
+        this.brakeNormal = -1.0;
+        this.brakeStrong = -2.0;
+        this.sightDistance = 150.0;
+        this.overtakeThreshold = 0.7;
     }
-    
-    @Override
-    protected void handleFreeWay(Vehicle self) {
-    	double targetSpeed = self.getMaxSpeed() * SPEED_RATIO_NORMAL;
-    	
-    	if (self.getSpeed() < targetSpeed) self.applyAcceleration(ACC_NORMAL);     
-        else if (self.getSpeed() > targetSpeed) self.applyAcceleration(BRAKE_NORMAL);
-        else self.applyAcceleration(0.0);
-    }
-    
-    @Override
-    protected void handleFollowVehicle(Vehicle self, Vehicle ahead) {
-        double dist = self.getPosition().distanceTo(ahead.getPosition());
-        
-        double safetyGap = self.getSpeed() * SAFE_TIME_GAP;
-
-        if (dist < safetyGap) {
-            double brakeForce = (self.getSpeed() - ahead.getSpeed()) > 0 ? BRAKE_STRONG : BRAKE_NORMAL;
-            self.applyAcceleration(brakeForce);
-        } 
-        else {
-            double targetSpeed = Math.min(self.getMaxSpeed() * SPEED_RATIO_NORMAL, ahead.getSpeed());
-            if (self.getSpeed() < targetSpeed) self.applyAcceleration(ACC_NORMAL);    
-            else self.applyAcceleration(0.0);
-        }
-    }
-    
-    @Override
-    protected void handleRedLight(Vehicle self, double distance) {
-        if (distance > 40.0) {
-            self.applyAcceleration(-2.0);
-        } 
-        else if (distance > 5.0) {
-            self.applyAcceleration(-4.0);
-        } 
-        else {
-            self.setSpeed(0);
-            self.applyAcceleration(0);
-        }
-    }
-    
-    @Override
-    protected void handleEmergency(Vehicle self) {
-        self.changeLane(1); 
-    }
-
+   
     @Override
     public String getBehaviorName() {
-        return "Normal";
+    	return "Normal";
     }
 }
